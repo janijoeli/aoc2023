49856e7bf9d20fe3d944e9d3655a767e445cd540
Jani Joeli <jani.joeli@gmail.com>
2023-12-03 00:11:46 +0200

    Day 01 Part 1

--- day01.asm
+++ day01.asm
@@ -1,118 +1,95 @@
-.const sum1_ptr		= $2
-.const sum2_ptr		= $4
-.const zp_buffer	= $10
-.const input_ptr	= $39	// input data pointer on ZP
+.namespace zp {
+	.label tens			= $403
+	.label ones			= $404
+	.label input_ptr	= $39	// input data pointer on ZP, initialised by line number in basic header
+}
 
+.const sum			= $0428	// pointer to sum on screen
 .const LF			= $0a	// ASCII code for Line Feed
 
 * = $0801 "Basic Header"
-				.word init, input; .byte $9e; .text "2061"; .byte 0,0,0
+					.word init, input; .byte $9e; .text "2061"; .byte 0,0,0
 
-* = * "Find Three Highest Calorie Sums"
-init:			sei
-				jsr $e544
-				ldy #4
-				sty sum1_ptr+1
-				sty sum2_ptr+1
-				iny					// init highest three sums to 00000
-				lda #'0'
-			!:	sta (sum1_ptr),y
-				sta $0428,y
-				sta $0450,y
-				sta $0478,y
-				dey
-				bpl !-
-				iny
-				sty zp_buffer-1		// 5th number is always 0, if read only 4 numbers to the buffer
+* = * "Calculate sum of calibration numbers"
+init:				sei
+					jsr $e544
 
-next_set:		lda #0
-				sta sum1_ptr
-				lda #40
-				sta sum2_ptr
-				lda #'0'			// reset 1st sum to 00000
-				ldy #5
-			!:	sta (sum1_ptr),y
-				dey
-				bne !-
+					lda #'0'			// init the total sum on screen to 00000
+					ldy #4
+				!:	sta sum,y
+					dey
+					bpl !-
 
-	next_line:	ldx #0
-	get_byte:	lda (input_ptr),y
-				beq calc_result		// If 0, end of input -> calculate result
-				iny
-				cmp #LF				// End of line?
-				beq read_done
-				sbc #$30			// carry always set here
-				sta zp_buffer,x
-				inx
-				bne get_byte
-	
-	read_done:	tya					// add Y (# of bytes read) to the input table pointer
-				clc
-				adc input_ptr
-				sta input_ptr
-				bcc !+
-				inc input_ptr+1
-			!:	dex
-				bmi cmp_sums		// If X was 0 before dex, no numbers were read, set is complete
+next_line:
+					ldy #0
+read_1st_digit:
+					lda (zp.input_ptr),y
+					beq end				// if 0, we've processed all input
+					iny
+					cmp #$3a
+					bcs read_1st_digit
+					sta zp.tens
+					sta zp.ones			// hold digit in X for now, as there could be only one digit in the string
 
-				ldy #5				// If set is not complete, add the sum in buffer to total sum
-				clc
-			!:	lda zp_buffer,x		// with 4-digit nums, X rolls over to $ff -> lda zp_buffer-1 = 0
-				dex
-				adc (sum1_ptr),y	// add to whatever result we already have
-				cmp #$3a			// ≥10 with the addition?
-				bcc !+				// if not, skip the subtraction
-				sbc #$0a			// subtract 10
-			!:	sta (sum1_ptr),y
-				dey
-				bne !--				// loop until all digits have been updated
-				beq next_line		// Done, read next line
+read_2nd_digit:
+					lda (zp.input_ptr),y
+					iny
+					cmp #$3a
+					bcs read_2nd_digit
+					cmp #LF				// if read char is line feed, reading digits is done
+					beq update_input_ptr
+					sta zp.ones			// found another digit
+					jmp read_2nd_digit	// continue, as the above may not be the last digit
 
-cmp_sums:		ldy #0
-			!:	iny
-				cpy #6
-				beq next_set		// sum1 = sum2, we're done
-				lda (sum2_ptr),y
-				cmp (sum1_ptr),y
-				beq !-				// num1 = num2, read next num
-				bcs next_set		// sum1 < sum2, we're done
-	swap_sums:	ldy #5				// sum1 > sum2, swap
-			!:	lda (sum2_ptr),y
-				pha
-				lda (sum1_ptr),y
-				sta (sum2_ptr),y
-				pla
-				sta (sum1_ptr),y
-				dey
-				bne !-
-				lax sum2_ptr		// swap done, move sum pointers to next pair
-				cmp #120
-				beq next_set		// compared all sums, we're done
-				sta sum1_ptr
-				sbx #256-40
-				stx sum2_ptr
-				bne cmp_sums
 
-calc_result:	ldy #5
-				clc
-			!:	lda (sum2_ptr),y
-				adc (sum1_ptr),y
-				sbc #$2f			// carry always clear, effectively sbc $30
-				cmp #$3a			// ≥10 with the addition?
-				bcc !+				// if not, skip the subtraction
-				sbc #$0a			// subtract 10
-			!:	sta (sum1_ptr),y
-				dey
-				bpl !--				// loop until all digits have been updated
-				lax sum2_ptr		// adding to sum1 done, move sum2 pointer to next line
-				cmp #120
-			end:beq end
-				sbx #256-40			// add 40 to sum2 pointer
-				stx sum2_ptr
-				bne calc_result
+update_input_ptr:
+					tya					// update input_ptr to point to next line in file
+					clc
+					adc zp.input_ptr
+					sta zp.input_ptr
+					bcc update_sum
+					inc zp.input_ptr+1
+
+update_sum:			lda zp.ones			// A = ones
+					ldy #4				// Y points to the least significant digit (ones)
+					jsr do_update_sum		// A holds value to add (plus $30), Y holds index of digit to add to
+
+					lda zp.tens
+					ldy #3
+					jsr do_update_sum
+
+					jmp next_line
+
+			end:	jmp *
+
+
+
+
+do_update_sum:		sec					// deduct $30 from value (converts petscii code to actual value)
+					sbc #$30
+	add_to_digit:	clc
+					adc sum,y			// Add new value to existing one
+					cmp #$3a			// Did digit roll over?
+					bcc no_rollover		// If not, branch
+					sbc #10				// digit rolled over, deduct 10 from value
+					sta sum,y			// save new digit value
+					dey
+					bmi done			// Branch if all digits are now updated
+					lda #1				// Add one to digit left of updated
+					bvc add_to_digit	// always branch
+	no_rollover:	sta sum,y			// save value of last digit to update
+	done:			rts
 
 * = * "Input Data"
-input:			.import binary "input/day01_input.txt"
-				.byte LF
-				.byte 0 // End of table
+input:
+					// .import binary "input/day01_input_test.txt"
+					// 22
+					// 22
+					// 95
+					// 2l
+					// 78
+					//=238
+
+					.import binary "input/day01_input.txt"
+					.byte 0 // End of table
 * = * "End"
